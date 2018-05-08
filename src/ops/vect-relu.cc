ff316790f0f84d9b1a789e8df82172d637b11329
Nacim Arrahmane <narrahmane@gmail.com>
2018-05-08 13:31:24 +0200

    ReLU vector implementation

---
+++ src/ops/vect-relu.cc
@@ -0,0 +1,28 @@
+#include "vect-relu.hh"
+#include "graph.hh"
+#include "../runtime/node.hh"
+#include "../memory/alloc.hh"
+
+namespace ops
+{
+
+    VectRelu::VectRelu(Op* arg)
+        : Op(arg->shape_get(), {arg})
+    {}
+
+    void VectRelu::compile()
+    {
+        auto& g = Graph::instance();
+        auto& carg = g.compiled(preds()[0]);
+
+        std::size_t len = carg.out_shape.total();
+        Shape out_shape = carg.out_shape;
+        dbl_t* out_data = tensor_alloc(len);
+
+        auto out_node = rt::Node::op_relu(carg.out_data, out_data,
+                                          len,
+                                          {carg.out_node});
+
+        g.add_compiled(this, {out_node}, {out_data}, out_node, out_shape, out_data);
+    }
+}
