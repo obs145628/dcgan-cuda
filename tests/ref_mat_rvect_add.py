80ddf76e00085b0d0c76cdbf88a0ae6833f1a4db
Steven Lariau <obs145628@gmail.com>
2018-05-08 17:06:35 +0200

    basic tests for every ops added

---
+++ tests/ref_mat_rvect_add.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import numpy as np
+import tensorflow as tf
+import tensors_saver
+
+tensors_saver.set_out_path(sys.argv[1])
+
+a = np.array([
+    [1., 2, 4],
+    [4.1, 0.5, 7],
+    [2, 2, 8],
+    [5, 2.3, 1.1]
+])
+
+b = np.array([
+    [1., 5., 3.],
+])
+
+a_node = tf.Variable(a, dtype=tf.float32)
+b_node = tf.Variable(b, dtype=tf.float32)
+res_node = a_node + b_node
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+tf_res = sess.run(res_node)
+tensors_saver.add(tf_res.astype(np.float32))
