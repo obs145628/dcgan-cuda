621bc14c810231503b7e2b3ca50c65d571407711
Steven Lariau <obs145628@gmail.com>
2018-05-10 18:53:36 +0200

    add mnist example

--- examples/nn_mnist.cc
+++ examples/nn_mnist.cc
@@ -22,6 +22,13 @@ int main(int argc, char** argv)
 	return 1;
     }
 
+    std::size_t batch_size = 100;
+
+    std::vector<dbl_t> x_batch_vect(784 * batch_size);
+    std::vector<dbl_t> y_batch_vect(10 * batch_size);
+    dbl_t* x_batch = &x_batch_vect[0];
+    dbl_t* y_batch = &y_batch_vect[0];
+
     dbl_t* x_train;
     dbl_t* y_train;
     mnist::load(argv[1], &x_train, &y_train);
@@ -37,17 +44,26 @@ int main(int argc, char** argv)
     auto l2 = dense_layer(l1, 100, 10, nullptr);
     auto loss = softmax_cross_entropy(y, l2);
 
-    SGDOptimizer optimizer(0.5 / 100);
+    SGDOptimizer optimizer(0.05);
     auto train_op = optimizer.minimize(loss);
 
     dbl_t loss_val;
 
-    for (int i = 0; i < 100; ++i)
+    for (int i = 0; i < 1000; ++i)
     {
 
+        for (std::size_t i = 0; i < batch_size; ++i)
+        {
+            int n = rand() % 70000;
+
+            std::copy(x_train + 784 * n, x_train + 784 * (n + 1), x_batch + 784 * i);
+            std::copy(y_train + 10 * n, y_train + 10 * (n + 1), y_batch + 10 * i);
+        }
+        
+
         graph.run({train_op, loss},
-                  {{x, {x_train, ops::Shape({100, 784})}},
-                      {y, {y_train, ops::Shape({100, 10})}}},
+                  {{x, {x_batch, ops::Shape({100, 784})}},
+                      {y, {y_batch, ops::Shape({100, 10})}}},
                   {nullptr, &loss_val});
 
         std::cout << "epoch " << i << ", "
