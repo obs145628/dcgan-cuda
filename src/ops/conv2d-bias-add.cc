6bbf8292d3d161d24d80f15ca79caacd3a9bf3be
Timothee Brenet <timothee.brenet@epita.fr>
2018-05-10 13:48:15 +0200

    Fix CMake + conv2d_bias_add

--- src/ops/conv2d-bias-add.cc
+++ src/ops/conv2d-bias-add.cc
@@ -8,7 +8,8 @@ namespace ops
 {
 
     Conv2DBiasAdd::Conv2DBiasAdd(Op* z, Op* bias)
-        : Op(Shape({z->shape_get()[0], z->shape_get()[1],
+        : Op("conv2d_bias_add",
+             Shape({z->shape_get()[0], z->shape_get()[1],
                     z->shape_get()[2], z->shape_get()[3]}),
              {z, bias})
     {}
@@ -18,14 +19,14 @@ namespace ops
         auto& g = Graph::instance();
         auto& cz =  g.compiled(preds()[0]);
         auto& cbias =  g.compiled(preds()[1]);
-        
-        Shape out_shape({int(cz.out_shape[0]), int(cz.out_shape[1]), 
+
+        Shape out_shape({int(cz.out_shape[0]), int(cz.out_shape[1]),
                          int(cz.out_shape[2]), int(cz.out_shape[3])});
         dbl_t* out_data = tensor_alloc(out_shape.total());
-        
+
         int input_size[4] = { cz.out_shape[0], cz.out_shape[1],
                               cz.out_shape[2], cz.out_shape[3]};
-        
+
         auto out_node = rt::Node::op_conv2d_bias_add(cz.out_data, cbias.out_data,
                                                      out_data, input_size,
                                                     {cz.out_node, cbias.out_node});
