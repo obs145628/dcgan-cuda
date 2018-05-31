bee907bb1b7320c6e0ac8a93ffb233e4e92fb874
Steven Lariau <obs145628@gmail.com>
2018-05-31 15:55:15 +0200

    relu grad done

--- src/gpu/relu.cu
+++ src/gpu/relu.cu
@@ -34,6 +34,16 @@ namespace gpu
         }
 
         __global__
+        void vect_relu_grad(const dbl_t* z, const dbl_t* dout, dbl_t* out, std::size_t len)
+        {
+            std::size_t index = blockIdx.x * blockDim.x + threadIdx.x;
+            std::size_t stride = blockDim.x * gridDim.x;
+
+            for (std::size_t i = index; i < len; i += stride)
+                out[i] = z[i] > 0 ? dout[i] : 0;
+        }
+
+        __global__
         void vect_relu_leaky(const dbl_t* x, dbl_t* y, dbl_t alpha, std::size_t len)
         {
             std::size_t index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -41,6 +51,17 @@ namespace gpu
 
             for (std::size_t i = index; i < len; i += stride)
                 y[i] = relu_leaky(x[i], alpha);
+        }
+
+        __global__
+        void vect_relu_leaky_grad(const dbl_t* z, const dbl_t* dout, dbl_t* out,
+                                  dbl_t alpha, std::size_t len)
+        {
+            std::size_t index = blockIdx.x * blockDim.x + threadIdx.x;
+            std::size_t stride = blockDim.x * gridDim.x;
+
+            for (std::size_t i = index; i < len; i += stride)
+                out[i] = z[i] < 0 ? alpha * dout[i] : dout[i];
         }
     }
         
@@ -54,6 +75,15 @@ namespace gpu
         vect_relu<<<nb_blocks, block_size>>>(node->in1, node->out1, len);
     }
 
+    void kernel_relu_grad(rt::Node* node)
+    {
+        std::size_t len = node->len1;
+        std::size_t block_size = 256;
+        std::size_t nb_blocks = (len + block_size - 1)/ block_size;
+
+        vect_relu_grad<<<nb_blocks, block_size>>>(node->in1, node->in2, node->out1, len);
+    }
+
     void kernel_relu_leaky(rt::Node* node)
     {
         std::size_t len = node->len1;
@@ -62,6 +92,16 @@ namespace gpu
 
         vect_relu_leaky<<<nb_blocks, block_size>>>(node->in1, node->out1,
                                                    node->alpha_leaky, len);
+    }
+
+    void kernel_leaky_relu_grad(rt::Node* node)
+    {
+        std::size_t len = node->len1;
+        std::size_t block_size = 256;
+        std::size_t nb_blocks = (len + block_size - 1)/ block_size;
+
+        vect_relu_leaky_grad<<<nb_blocks, block_size>>>(node->in1, node->in2, node->out1,
+                                                        node->cons1, len);
     }
 
 }
--- src/gpu/relu.hh
+++ src/gpu/relu.hh
@@ -3,5 +3,7 @@
 namespace gpu
 {
     void kernel_relu(rt::Node* node);
+    void kernel_relu_grad(rt::Node* node);
     void kernel_relu_leaky(rt::Node* node);
+    void kernel_leaky_relu_grad(rt::Node* node);
 }
